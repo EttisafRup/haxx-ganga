7972fa296bd6f9cf436e167aa3a3e7e433cdd3e3
Ettisaf Rup <107711625+EttisafRup@users.noreply.github.com>
2022-11-21 14:07:11 +0000

    Tools Fetchup

---
+++ src/Components/Common/Tools.tsx
@@ -0,0 +1,54 @@
+import axios from "axios"
+import React, { useState } from "react"
+import env from "../../env/env"
+
+const Tools = () => {
+  const [tools, setTools] = useState<any[]>([])
+
+  axios
+    .get(env.server + "/tools")
+    .then((data) => {
+      setTools(data.data.tools)
+    })
+    .catch((err) => console.log(err))
+
+  return (
+    <div>
+      <big className="text-2xl font-semibold text-black mx-3">
+        <span className="text-red-600">A</span>vailable Tools :
+      </big>
+      <p className="m-5"></p>
+
+      <div className="flex flex-wrap mx-3">
+        {tools.map((elem) => {
+          return (
+            <div className="cursor-pointer p-2 lg:w-1/3 md:w-1/2 w-full">
+              <a href={elem.path}>
+                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img
+                    alt="team"
+                    className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                    src={elem.image}
+                  />
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 title-font font-medium">
+                      {elem.name}
+                    </h2>
+                    <p className="text-gray-500">{elem.description}</p>
+                    <a href={elem.author.media}>
+                      <strong className="text-gray-500">
+                        🎆 {elem.author.name}
+                      </strong>
+                    </a>
+                  </div>
+                </div>
+              </a>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Tools
--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,10 +1,13 @@
+import axios from "axios"
 import { useEffect } from "react"
 import env from "../env/env"
+import Tools from "./Common/Tools"
 
 const Home = () => {
   useEffect(() => {
     document.title = `Home - ${env.app}`
   })
+
   return (
     <>
       <section className="mt-12 md:mt-0 home text-gray-600 body-font">
@@ -17,38 +20,7 @@ const Home = () => {
           </p>
         </div>
         <p className="m-5"></p>
-        <big className="text-2xl font-semibold text-black mx-3">
-          <span className="text-red-600">A</span>vailable Tools :
-        </big>
-        <p className="m-5"></p>
-        <div className="flex flex-wrap mx-3">
-          {env.tools.map((elem) => {
-            return (
-              <div className="cursor-pointer p-2 lg:w-1/3 md:w-1/2 w-full">
-                <a href={elem.path}>
-                  <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                    <img
-                      alt="team"
-                      className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                      src={elem.image}
-                    />
-                    <div className="flex-grow">
-                      <h2 className="text-gray-900 title-font font-medium">
-                        {elem.name}
-                      </h2>
-                      <p className="text-gray-500">{elem.description}</p>
-                      <a href={elem.author.media}>
-                        <strong className="text-gray-500">
-                          🎆 {elem.author.name}
-                        </strong>
-                      </a>
-                    </div>
-                  </div>
-                </a>
-              </div>
-            )
-          })}
-        </div>
+        <Tools />
       </section>
     </>
   )
